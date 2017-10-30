a77b74ebbb6b8d86b2e45f50f5ea219ba364a82e
Girish <girish@post.com>
2017-10-30 14:52:50 -0700

    Added NavigationMenuViewComponent

--- SportsStore/Models/EFProductRepository.cs
+++ SportsStore/Models/EFProductRepository.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 
-namespace SportsStore.Models {
+namespace SportsStore.Models
+{
 
     public class EFProductRepository : IProductRepository {
         private ApplicationDbContext context;
