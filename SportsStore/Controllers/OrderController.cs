a4aac32288df24eaace1a8b790eda52df530953f
Girish <girish@post.com>
2017-10-31 09:53:19 -0700

    added Checkout

---
+++ SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository orderRepository;
+        private readonly Cart cart;
+
+        public OrderController(IOrderRepository orderRepository, Cart cart)
+        {
+            this.orderRepository = orderRepository;
+            this.cart = cart;
+        }
+        public ViewResult Checkout() => View(new Order());
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty");
+            }
+            if (ModelState.IsValid)
+            {
+                order.Lines = cart.Lines.ToArray();
+                orderRepository.SaveOrder(order);
+                return RedirectToAction(nameof(Completed));
+            }
+            else
+            {
+                return View(order);
+            }
+        }
+        public ViewResult Completed()
+        {
+            cart.Clear();
+            return View();
+        }
+    }
+}
